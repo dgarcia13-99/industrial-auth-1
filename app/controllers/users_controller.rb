7abeb569c1dd704cddbd8b11c64c8498dd2bd273
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-10-31 17:01:00 +0000

    added authorization to user 'feed' and 'discover' links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :ensure_current_user_is_owner, only: [:feed, :discover]
 
   private
 
@@ -10,4 +11,11 @@ class UsersController < ApplicationController
         @user = current_user
       end
     end
+
+    def ensure_current_user_is_owner
+      if current_user != @user
+        redirect_back fallback_location: root_url, alert: "You're not authorized for that."
+      end
+    end
+
 end
